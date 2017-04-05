63366ab688958f80c358c1be43c80352035c43cf
drgentzyel <drgentzyel@mix.wvu.edu>
2017-04-05 16:24:01 -0400

    Added Comments
    Commented every line for both client and server

--- CameraClient.py
+++ CameraClient.py
@@ -11,39 +11,55 @@ SHRUNK_WIDTH = 170
 SHRUNK_HEIGHT = 127
 CAMERA_FPS = 10
 
+# Opening the front and rear camera
 cap_front = cv2.VideoCapture(0)
 cap_rear = cv2.VideoCapture(1)
 
+# Adjust Camera FPS
 cap_front.set(cv2.CAP_PROP_FPS, CAMERA_FPS)
 cap_rear.set(cv2.CAP_PROP_FPS, CAMERA_FPS)
 
+# Create socket
 clientsocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
+# Info on how to close the client
 print ('Be sure to close server before closing client')
 print ('Press ctrl-c to close client')
 
-
+# main loop
 try:
 	while True:
 	#=================================================================
 	# Front Camera
+		# Grab frame from front camera
 		ret_front,frame_front = cap_front.read()
+		
+		# resize frame to a smaller size, uses INTER_AREA interpolation
 		data_front = cv2.resize(frame_front, (SHRUNK_HEIGHT, SHRUNK_WIDTH), interpolation = cv2.INTER_AREA)
+		
+		# Create list of data with which camera its from and the frame info
 		dataToSend_front = pickle.dumps([0, data_front])
+		
+		# Send packet
 		clientsocket.sendto(dataToSend_front, (UDP_IP, UDP_PORT))
 	#=================================================================
 	
 	#=================================================================
 	# Rear Camera
+		# Grab frame from rear camera
 		ret_rear,frame_rear = cap_rear.read()
+		
+		# resize frame to a smaller size, uses INTER_AREA interpolation
 		data_rear = cv2.resize(frame_rear, (SHRUNK_HEIGHT, SHRUNK_WIDTH), interpolation = cv2.INTER_AREA)
+		
+		# Create list of data with which camera its from and the frame info
 		dataToSend_rear = pickle.dumps([1, data_rear])
+		
+		# Send Packet
 		clientsocket.sendto(dataToSend_rear, (UDP_IP, UDP_PORT))
 	#=================================================================
 	
 except KeyboardInterrupt:
-	cap_front.release()
-	cap_rear.release()
 	print('Closing the Client')
 	print('Client Closed')
 	pass
--- CameraServer.py
+++ CameraServer.py
@@ -1,4 +1,3 @@
-
 import socket
 import sys
 import cv2
@@ -14,32 +13,39 @@ SHRUNK_HEIGHT = 127
 SHRUNK_WIDTH = 170
 LARGE_WIDTH = 640
 LARGE_HEIGHT = 480
+data = ""
 
+# Creates 2 named windows, one for the front camera and one for the rear camera
 cv2.namedWindow("Front Camera")
 cv2.namedWindow("Rear Camera")
 
+# Creates a socket s, and displays a message that it has been created
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 print ('Socket Created')
 
+# Binds socket s to an ip address and port
 s.bind((HOST, PORT))
 print ('Socket Bind Complete')
 
+# Text describing how to close the server
 print ('Press esc to close stream')
 print ('Please close server before closing the client')
 
-data = ""
-
+# Main loop for the server
 while True:
-# ======================================================================
-# Front Camera
-	try:
-		data_front, addr = s.recvfrom(DATA_SIZE)
-	except ExceptionI:
-		print ('Failed to recieve front camera data')
+	# Recieve packet from Client
+	data_front, addr = s.recvfrom(DATA_SIZE)
+	
+	# Store packet into stored_data
 	stored_data = pickle.loads(data_front)
 	
+	# Recieve Frame Info from packet
 	frame_front = np.fromstring(stored_data[1], dtype = np.uint8)
+	
+	# Reshape data into proper picture size
 	frame_front = frame_front.reshape(SHRUNK_WIDTH, SHRUNK_HEIGHT, 3)
+	
+	# Resize picture to be actually viewable, uses INTER_CUBIC interpolation
 	frame_front = cv2.resize(frame_front, (LARGE_WIDTH, LARGE_HEIGHT), interpolation = cv2.INTER_CUBIC)
 	
 	#========================================================
@@ -48,42 +54,14 @@ while True:
 		frame_front = cv2.flip(frame_front,0,1)
 	#========================================================
 	
+	# Decode which camera frame is from and display it to the proper window
 	if stored_data[0] == 0:
 		cv2.imshow("Front Camera", frame_front)
 	else:
 		cv2.imshow("Rear Camera", frame_front)
-	
-	
-	
-	#cv2.imshow("Front Camera", frame_front)
-#============================================================================
 
-
-
-
-
-#============================================================================
-# Rear Camera	
-#	try:
-#		data_rear, addr = s.recvfrom(DATA_SIZE)
-#	except ExceptionI:
-#		print ('Failed to recieve front camera data')
-#	
-#	frame_rear = np.fromstring(data_rear, dtype = np.uint8)
-#	frame_rear = frame_rear.reshape(SHRUNK_WIDTH, SHRUNK_HEIGHT, 3)
-#	frame_rear = cv2.resize(frame_rear, (LARGE_WIDTH, LARGE_HEIGHT), interpolation = cv2.INTER_CUBIC)
-#	
-#	#========================================================
-#	# This if block is used to flip the image on my tablet
-#	if platform.machine() == 'AMD64':
-#		frame_rear = cv2.flip(frame_rear,0,1)
-#	#========================================================
-#	
-#	cv2.imshow("Rear Camera", frame_rear)
-#============================================================================
-
-	if cv2.waitKey(1) == 27: 
+	# Checks if esc has been pressed and closes out program if it has
+	if cv2.waitKey(5) == 27: 
 		print ('Closing the Server')
 		print ('Server Closed')
 		break  # esc to quit
-	#cv2.waitKey(4)
